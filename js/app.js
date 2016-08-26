60d19b088d1a8f7c56d77b2da5884f37dfaddbb5
Navneeth Gopalakrishnan <navneeth@backbench.io>
2016-08-26 20:10:59 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -70,6 +70,8 @@
         }
         
         $RefParser.dereference(schema).then(function(resolvedSchema) {
+              //Prevent circular references.
+              resolvedSchema = JSON.parse(stringify(resolvedSchema));
               JSV.init({
                 plain: true,
                 schema: resolvedSchema,
@@ -79,9 +81,37 @@
                 $('#jsv-tree').css('width', '100%');
                 JSV.resizeViewer();
             });
+        }).catch(function(err) {
+            alert(err);
         });
         
     }
+    
+    /* json-stringify-safe*/
+    function stringify(obj, replacer, spaces, cycleReplacer) {
+  return JSON.stringify(obj, serializer(replacer, cycleReplacer), spaces)
+}
+
+    function serializer(replacer, cycleReplacer) {
+  var stack = [], keys = []
+
+  if (cycleReplacer == null) cycleReplacer = function(key, value) {
+    if (stack[0] === value) return "[Circular ~]"
+    return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]"
+  }
+
+  return function(key, value) {
+    if (stack.length > 0) {
+      var thisPos = stack.indexOf(this)
+      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this)
+      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
+      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
+    }
+    else stack.push(value)
+
+    return replacer == null ? value : replacer.call(this, key, value)
+  }
+}
 
     $(init);
     
